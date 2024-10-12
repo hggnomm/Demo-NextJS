5b51ef0440cfbc0407d2d42d3923484e0acaf65a
hggnomm <hggnomm.dev@gmail.com>
2024-10-12 23:41:24 +0700

    Using SWR Auto Revalidation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,25 @@ import Link from "next/link";
 import x from "@/styles/app.module.css";
 import y from "@/styles/namdeptrai.module.css";
 import Table from "@/components/table/table";
-import { useEffect } from "react";
+import useSWR from "swr";
+import useSWRImmutable from "swr/immutable";
 
 export default function Home() {
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("http://localhost:8000/blogs");
-      console.log(await res.json());
-    };
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-    fetchData();
-  }, []);
-
+  const { data, error, isLoading } = useSWR(
+    "http://localhost:8000/blogs",
+    fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false, 
+      // params using SWR. When catching data first, if the data does not change, when returning to the previous page, there will be no fetch api.
+    },
+  );
   return (
     <div>
+      <div>{data?.length}</div>
       <ul>
         <li className={x.red}>
           <Link href={"/facebook"}>
