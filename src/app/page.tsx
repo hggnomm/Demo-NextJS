4465d362818f623c6b6ae999bb510d6c9ae45029
hggnomm <hggnomm.dev@gmail.com>
2024-10-12 23:05:06 +0700

    change nav, fetch api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+"use client";
 import Link from "next/link";
 import x from "@/styles/app.module.css";
 import y from "@/styles/namdeptrai.module.css";
 import Table from "@/components/table/table";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("http://localhost:8000/blogs");
+      console.log(await res.json());
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
       <ul>
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,28 +1,17 @@
-'use client'
+"use client";
+import Link from "next/link";
 import React from "react";
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const Header = () => {
   return (
     <Navbar bg="white" expand="lg" className="bg-body-black">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">Nam Đẹp Trai</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Link className="nav-link" href={"/facebook"}>Facebook</Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
